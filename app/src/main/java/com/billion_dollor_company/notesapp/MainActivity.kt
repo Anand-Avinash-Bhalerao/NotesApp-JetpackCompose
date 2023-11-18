6413c2aff328ba41ea9935f7335a91eae15b81c5
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-11-18 19:45:35 +0530

    Splash Screen added

--- app/src/main/java/com/billion_dollor_company/notesapp/MainActivity.kt
+++ app/src/main/java/com/billion_dollor_company/notesapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.billion_dollor_company.notesapp.ui.theme.NotesAppTheme
 import com.billion_dollor_company.notesapp.util.Navigation
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,6 +15,7 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NotesAppTheme {
                 Navigation()
